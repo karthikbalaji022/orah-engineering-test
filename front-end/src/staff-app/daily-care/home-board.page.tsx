bbb0e45dc601108c029ffb44b5f7256eddf7cb74
karthikbalaji022 <karthikbalaji022@gmail.com>
2022-06-01 18:30:36 +0530

    implemented debounce search function

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, ReactElement } from "react"
 import styled from "styled-components"
 import Button from "@material-ui/core/ButtonBase"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+//kb edit search font icon
+import { FaSearch } from "react-icons/fa";
 import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
 import { Colors } from "shared/styles/colors"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
@@ -13,11 +15,22 @@ import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active
 export const HomeBoardPage: React.FC = () => {
   const [isRollMode, setIsRollMode] = useState(false)
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
-
+//implemented debounce state for searching
+  const [students,setStudents]=useState(data?.students) ;
+  const [debounce,setDebounce]=useState("");
   useEffect(() => {
     void getStudents()
   }, [getStudents])
 
+  useEffect(()=>{
+    setStudents(data?.students)
+  },[data]);
+
+  // debounce functionality
+  useEffect(()=>{
+    const time=setTimeout(searchData,500);
+    return ()=>clearTimeout(time);
+  },[debounce])
   const onToolbarAction = (action: ToolbarAction) => {
     if (action === "roll") {
       setIsRollMode(true)
@@ -29,11 +42,21 @@ export const HomeBoardPage: React.FC = () => {
       setIsRollMode(false)
     }
   }
-
+  //implemented the call from debounce to  search functionality
+  const searchData= (e : React.ChangeEvent<HTMLInputElement>)=>{
+    if(data )
+    {
+      const searchData=data.students.filter((item)=>{
+        const name=item.first_name.toLowerCase()+" "+item.last_name.toLowerCase();
+        return name.includes(debounce?debounce.toLowerCase():"");
+      })
+      setStudents(searchData);
+    }
+  }
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} />
+        <Toolbar onItemClick={onToolbarAction} debounceFun={setDebounce}/>
 
         {loadState === "loading" && (
           <CenteredContainer>
@@ -43,7 +66,7 @@ export const HomeBoardPage: React.FC = () => {
 
         {loadState === "loaded" && data?.students && (
           <>
-            {data.students.map((s) => (
+            {students?.map((s) => (
               <StudentListTile key={s.id} isRollMode={isRollMode} student={s} />
             ))}
           </>
@@ -62,18 +85,27 @@ export const HomeBoardPage: React.FC = () => {
 
 type ToolbarAction = "roll" | "sort"
 interface ToolbarProps {
-  onItemClick: (action: ToolbarAction, value?: string) => void
+  onItemClick: (action: ToolbarAction, value?: string) => void,
+  debounceFun: (value: React.SetStateAction<string> )=> void
 }
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
+  const { onItemClick,debounceFun } = props
   return (
     <S.ToolbarContainer>
       <div onClick={() => onItemClick("sort")}>First Name</div>
-      <div>Search</div>
+      <S.Search>
+      <FaSearch/>
+      <input type="text" 
+             placeholder="Search"
+            onChange={(e)=>debounceFun(e.target.value)}
+             style={{width:"100%",height:"20px",borderRadius:"5px",border:"none",outline:"none"}}
+      />
+      </S.Search>
       <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
     </S.ToolbarContainer>
   )
 }
+
 
 const S = {
   PageContainer: styled.div`
@@ -83,9 +115,10 @@ const S = {
     margin: ${Spacing.u4} auto 140px;
   `,
   ToolbarContainer: styled.div`
-    display: flex;
-    justify-content: space-between;
+    display: grid;
+    justify-content: space-evenly;
     align-items: center;
+    grid-template-columns: 1fr 3fr 1fr;
     color: #fff;
     background-color: ${Colors.blue.base};
     padding: 6px 14px;
@@ -99,4 +132,13 @@ const S = {
       border-radius: ${BorderRadius.default};
     }
   `,
+  Search: styled.div`
+  display: flex;
+  gap:5px;
+  height:100%;
+  width:100%;
+  justify-content: center;
+  align-items: center;
+  padding:10px;
+  `
 }
